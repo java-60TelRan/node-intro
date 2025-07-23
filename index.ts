ca4b3ba266dc42978941aac844f84871a5428a37
Yuragranovsky <yuragranovsky@gmail.com>
2025-07-23 20:07:46 +0300

    solving challenge from previous commit

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
+//FIXME 
+//A right solution allowing automation of writing data in Writable stream should be done using pipe
+// between Readable (as a producer) and Writable (as a consumer)
 import { createWriteStream } from "fs";
 const writeStream = createWriteStream("large_file",{highWaterMark: 1024 * 1024 * 10});
-const max = 1_000_000_000;
+const max = 1_000_000;
+let index = 0;
 
-for (let i = 0; i < max; i++) {
-    writeStream.write("Hello");
-    i += 5;
+function write() {
+    let canWrite = true;
+    while (canWrite && index < max) {
+        canWrite = writeStream.write("Hello".repeat(1000));
+        index += 5;
+    }
+    if(index < max) {
+        writeStream.once("drain", write)
+    }
+
 }
-writeStream.end(() => console.log("all data written"));
+write();
