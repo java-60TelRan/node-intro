ff00d54fe23bf8abe8c819e6e301d40bec2e1571
Yuragranovsky <yuragranovsky@gmail.com>
2025-07-27 20:48:52 +0300

    processing data inside request using adding listeners

--- index.ts
+++ index.ts
@@ -4,15 +4,8 @@ const port = 3500;
 server.listen(port, () => console.log('listening on port ' + port));
 server.on("request", (req, res) => {
   res.statusCode = 200;
-  const tokens = req.url.split("/");
-  let response:string = ""
-  switch(tokens[1]) {
-    case "add": response = tokens[2] + tokens[3]; break;
-    case "sub": response = (+tokens[2] - +tokens[3]).toString(); break;
-    case "div": response = (+tokens[2] / +tokens[3]).toString(); break;
-    case "mul": response = (+tokens[2] * +tokens[3]).toString(); break;
-  }
-  res.write(response);
-  res.end();
+  let data = "";
+  req.on("data", chunk => data += chunk);
+  req.on("end", () => {res.write(data); res.end()})
   
 })
